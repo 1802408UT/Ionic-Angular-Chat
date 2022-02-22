986c563185fd7585f8278e8e2c3e07e47889f6ff
Ivan Enrique Cordova Gomez <1802408@utrivieramaya.edu.mx>
2022-02-22 10:16:52 -0500

    Sistema de CRUD habitaciones y Usuarios

--- Socket Chat/index.js
+++ Socket Chat/index.js
@@ -1,46 +1,39 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const io = require('socket.io')(server);
-
-on('connection', (socket) => {
-    socket.on('disconnect', function() {
-        emit('users-changed', { user: socket.username, event: 'left' });
-    });
-});
-
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
-});
-
-server.listen(3000, () => {
-    console.log('API Trabajando en Localhost:3000');
-});
-
-
-/*
 const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http, {
+//const cors = require('cors');
+//const morgan = require('morgan');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
     cors: {
-        origin: true,
-        Credentials: true,
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        allowedHeaders: 'Content-Type, Authorization, Content-Length, X-Requested-With'
+        origin: '*',
+        methods: 'GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH, CONNECT, TRACE, UPDATE',
+        allowedHeader: 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        credentials: true,
+        maxAge: '3600'
     }
 });
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
+
+    socket.on('disconnect', function() {
+        io.emit('users-changed', { user: socket.username, event: 'left' });
+    });
+    socket.on('set-name', (name) => {
+        socket.username = name;
+        io.emit('users-changed', { user: name, event: 'joined' });
+    });
+
+    socket.on('send-message', (message) => {
+        io.emit('message', { msg: message.text, user: socket.username, createdAt: new Date() });
+    });
 });
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    res.send('<h1>Hello, This is a API of Chat</h1>');
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+//app.use(helmet());
 
-});*/
+var port = process.env.PORT || 3001;
+server.listen(port, function() {
+    console.log('listening in http://localhost:' + port);
+});
